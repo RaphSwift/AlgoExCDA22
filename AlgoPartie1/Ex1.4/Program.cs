3b8272a6456b24fcec377e570cbb84f62cac029a
Raph <30597735+RaphSwift@users.noreply.github.com>
2022-11-10 11:37:33 +0100

    Jeud10

---
+++ AlgoPartie1/Ex1.4/Program.cs
@@ -0,0 +1,51 @@
+﻿/*VARIABLES
+ * 
+ * float sumS; 
+ * float interestI;
+ * float numberYearsN;
+ * float interestSimple;
+ * float interestCompose;
+ * 
+ * INPUT
+ * 
+ * Write ("Enter the value during N years on a bank account. ");
+ * Read sumS;
+ * Write ("Enter the interest  i given by the bank.")
+ * Raed interestI;
+ * Write ("Enter the number of years of sumS placement.")
+ * Read numberYearsN;
+ * 
+ * PROCESS
+ * 
+ * InteretSimple <-- S ( 1 + n * i )
+    //InteretComposé <-- S ( 1 + i )n
+ * 
+ * OUTPUT
+ * 
+ * Write ("Calculate the " +sumS+ " value during " +numberYearsN+ " with " + interetSimple+ " et " + interetComposé");
+ */
+
+using System.Diagnostics.Metrics;
+
+float sumS;
+float interestI;
+float numberYearsN;
+float interestSimple;
+float interestCompose;
+
+
+Console.WriteLine("Enter the value during N years on a bank account.");
+sumS = float.Parse(Console.ReadLine());
+Console.WriteLine("Enter the interest  i given by the bank.");
+interestI = float.Parse(Console.ReadLine())/100;
+Console.WriteLine("Enter the number of years of sumS placement.");
+numberYearsN = float.Parse(Console.ReadLine());
+
+interestSimple = sumS * (1 + numberYearsN * interestI);
+interestCompose = sumS * MathF.Pow(1 + interestI, numberYearsN);
+
+Console.WriteLine( "Calculate the " + sumS + " value during " + numberYearsN + " with " + interestSimple + " et " + interestCompose);
+
+
+
+
---
+++ AlgoPartie1/Ex1.5/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * VARIABLES
+ * 
+ * entier numberA;
+ * entier numberB;
+ * entier numberC;
+ * 
+ * Process
+ * 
+ * Write ("Enter numberA");
+ * Read numberA;
+ * Write ("Enter numberB");
+ * Read numberB;
+ * 
+ * numberC <-- numberA;
+ * numberA <-- numberB;
+ * numberB <-- numberC;
+ * 
+ * OUTPUT
+ * Write ("The value numberA is : " + numberA);
+ * Write ("The value numberB is : " + numberB);
+ * 
+ */
+
+
+int numberA;
+int numberB;
+int numberC;
+
+Console.WriteLine("Enter numberA");
+numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter numberB");
+numberB = int.Parse(Console.ReadLine());
+
+numberC = numberA;
+numberA = numberB;
+numberB = numberC;
+
+Console.WriteLine("The value numberA is : " + numberA);
+Console.WriteLine("The value numberB is : " + numberB);
+
+
+
