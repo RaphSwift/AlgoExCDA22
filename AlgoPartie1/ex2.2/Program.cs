65af2ddfb9e632e5feba43ada587feeb31d6cc0c
Raph <30597735+RaphSwift@users.noreply.github.com>
2022-11-15 11:51:06 +0100

    15.11

---
+++ AlgoPartie1/ex2.2/Program.cs
@@ -0,0 +1,47 @@
+﻿/* Ex2.2
+ * 
+ * VARIABLES
+ * 
+ * entier numberA
+ * entier numberB
+ * 
+ * PROCESS
+ * 
+ * Write("Saisir A");
+ * Read(numberA);
+ *  Write("Saisir B");
+ * Read(numberB);
+ * 
+ * SI numberA<numberB
+ *      write(numberA,", ",numberB)
+ * SINON SI numberA>numberB
+ *      write(numberB,", ",numberA)
+ * SINON
+ *      write(numberA," = ",numberB)
+ * 
+ * FINSI
+ * 
+ * 
+ */
+
+int numberA;
+int numberB;
+
+Console.WriteLine("Saisir A");
+numberA=int.Parse(Console.ReadLine());
+
+Console.WriteLine("Saisir B");
+numberB = int.Parse(Console.ReadLine());
+
+if (numberA < numberB){
+    Console.WriteLine(numberA+ ", "+ numberB);
+}
+else if (numberA > numberB)
+{
+    Console.WriteLine(numberB+ ", "+ numberA);
+}
+else
+{
+    Console.WriteLine(numberA+ " = "+ numberB);
+}
+ 
---
+++ AlgoPartie1/ex3.1/Program.cs
@@ -0,0 +1,70 @@
+﻿/*ex3.1
+ * VARIABLES
+ * 
+ * entier numberMystery;
+ * entier tryNumber;
+ * entier limitMin;
+ * entier limitMax;
+ * entier userInput;
+ * 
+ * PROCESS
+ * 
+ * limitMin <-- 0
+ * limitMax <-- 100
+ * numberMystery <-- Random(limitMin,limitMax)
+ *  write ("Enter a numer between "limitMin" and "limitMax")
+ *  read userInput
+ *  tryNumber<--1
+ *  
+ *  while (userInput != numberMystery)
+ *  
+ *  if (userInput < numberMystery)
+ *      limitMin <--userInput
+ *  else 
+ *      limitMax<--userInput
+ *  ENDIF
+ * write ("Enter a numer between "limitMin" and "limitMax") 
+ * read userInput
+ * tryNumber++
+ * ENDWHILE
+ * 
+ * OUTPUT
+ * 
+ * Write ("You found after ",tryNumber, "try(ies)")
+ * 
+ */
+
+
+Random rand = new Random(); // random object creation
+int limitMin = 0;
+int limitMax = 100;
+int userInput;
+int tryNumber = 1;
+int numberMystery=rand.Next(limitMin,limitMax+1);// affectation du nombre myster
+Console.WriteLine("Enter a numer between " + limitMin + " and " + limitMax + ".");
+userInput = int.Parse(Console.ReadLine());
+
+while (userInput != numberMystery)
+{
+
+    if (userInput < numberMystery)
+    {
+        limitMin = userInput;
+    }
+    else {
+        limitMax = userInput;
+    }
+    Console.WriteLine("Enter a numer between "+limitMin+" and " +limitMax + ".");
+    userInput=int.Parse(Console.ReadLine());   
+
+    tryNumber++;
+}
+
+Console.WriteLine("You found after " + tryNumber + " try(ies).");
+
+
+
+
+
+
+
