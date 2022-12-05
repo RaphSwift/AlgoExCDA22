65efb8130bec444356e68ef31ff03d8b76613aa5
Raph <30597735+RaphSwift@users.noreply.github.com>
2022-12-05 12:31:54 +0100

    05.12

---
+++ AlgoPartie1/JalonAlgoFibonacci/Program.cs
@@ -0,0 +1,32 @@
+﻿//LA SUITE DE FIBONACCI
+
+
+int position = 2;
+int n;
+string resultat = "0\n1";
+int nbPrecedent = 0;
+int nbCourant = 1;
+int nbSuivant;
+
+Console.WriteLine("Combien de nombres de la suite de Fibonacci souhaitez vous afficher ?");
+n = int.Parse(Console.ReadLine());
+
+if (n > 2)
+{
+    while (position < n)
+    {
+        nbSuivant = nbPrecedent + nbCourant;
+        resultat = resultat + "\n" + nbSuivant;
+        nbPrecedent = nbCourant;
+        nbCourant = nbSuivant;
+        position++;
+    }
+}
+else
+{
+    n = 2;
+}
+
+
+Console.WriteLine("Les " + n + " premiers nombres de la suite de Fibonacci sont \n" + resultat);
+
---
+++ AlgoPartie1/ex2_7ConvKm/Program.cs
@@ -0,0 +1,27 @@
+﻿
+double value;
+double converter = 0.621;
+
+Console.WriteLine("Enter a value in km.");
+value = double.Parse(Console.ReadLine());
+
+
+do
+{
+    Console.WriteLine(value * converter + " miles");
+    Environment.Exit(0);
+}
+
+while (value > 0.01 || value < 1000000);
+
+       {
+        Console.WriteLine("The number must be between 0.01 and 1000000! Enter a new value.");
+    }
+    if (value == 'q')
+    {
+        Environment.Exit(0);
+    }
+    else if (value < 0.01 || value > 1000000)
+{
+
+}
